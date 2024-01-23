a1f659d31e89eebbd148deeb3cef8cb95ce53639
Pooja <Pooja@DESKTOP-665PVSU>
2024-01-23 10:50:08 +0530

    Uloaded Project

---
+++ src/test/java/BasicPom/TiraTest.java
@@ -0,0 +1,28 @@
+package BasicPom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TiraTest {
+	
+	 @Test()
+		public void PerfumeTest()	
+		{
+		 
+		  WebDriver driver=new ChromeDriver();
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		  driver.get("https://www.tirabeauty.com/");
+          WebElement serachtf = driver.findElement(By.id("search"));
+          serachtf.sendKeys("perfumes");
+          serachtf.sendKeys(Keys.ENTER);
+		
+		}
+
+}
