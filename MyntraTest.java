cfeddf5f764b3eddb1527dd822868b7d1f9d6177
Pshilomani <137377501+Pshilomani@users.noreply.github.com>
2024-01-23 10:27:25 +0530

    Add files via upload

---
+++ MyntraTest.java
@@ -0,0 +1,25 @@
+package BasicPom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyntraTest
+	{
+		  @Test()
+			public void LaunchTest()	
+			{
+			 
+			  WebDriver driver=new ChromeDriver();
+			  driver.manage().window().maximize();
+			  
+			  driver.get("https://www.myntra.com/");
+			  String actualtitile = driver.getTitle();
+			  System.out.println(actualtitile);
+			}
+
+	}
+
+	
+
+
